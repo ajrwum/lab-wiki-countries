918eec606a54ec0b20e25d95e0e9ec9658aa22e0
Ajr Wum <ajr.wum@gmail.com>
2022-01-27 18:34:05 +0100

    correct link in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <nav className="navbar navbar-dark bg-primary mb-3">
       <div className="container">
-        <a className="navbar-brand" href="/">WikiCountries</a>
+        <Link className="navbar-brand" to="/">WikiCountries</Link>
       </div>
     </nav>
   )
