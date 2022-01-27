55153d0cb50fc41a36fc95a83d345a09f1f05f2f
Ajr Wum <ajr.wum@gmail.com>
2022-01-27 19:17:51 +0100

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
 import { Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
 
-import countries from './countries.json';
+// import countries from './countries.json';
+import axios from 'axios';
 
 function App() {
+
+  // create state for rest-countries
+  const [restCountries, setRestCountries] = useState([]);
+  // getting the restCountries with help of useEffect (run only once)
+  useEffect(() => {
+    // Get the project by id from the server
+    axios.get('https://ih-countries-api.herokuapp.com/countries')
+      .then((response) => {
+        response.data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+        setRestCountries(response.data);
+      })
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
@@ -18,8 +34,8 @@ function App() {
         {/* <!-- Bootstrap row wrapper div --> */}
         <div className="row">
           <Routes>
-            <Route path='/' element={<CountriesList countries={countries} />} >
-              <Route path=':id' element={<CountryDetails countries={countries} />} />
+            <Route path='/' element={<CountriesList countries={restCountries} />} >
+              <Route path=':id' element={<CountryDetails countries={restCountries} />} />
             </Route>
           </Routes>
         </div>
